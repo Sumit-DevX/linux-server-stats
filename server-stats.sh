2c15cbacc14382eebfc2bf1facfbad9d956f8924
Sumit Nayak <sumit.debugged@gmail.com>
2026-05-09 18:12:43 +0530

    Add CPU and Memory usage monitoring

--- server-stats.sh
+++ server-stats.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+TOTAL_CPU_USAGE=$(top -bn1 | awk '/Cpu\(s\)/ {print 100 - $8}')
+
+TOTAL_MEM_USED=$(free | awk '/Mem/ && !/Swap/ {printf "%.2f", ($3/$2)*100}')
+
+FREE_MEM=$(free | awk '/Mem/ && !/Swap/ {printf "%.2f", ($4/$2)*100}')
+
+cat << EOF
+______________
+
+Total CPU Usage: ${TOTAL_CPU_USAGE}%
+
+Total Memory Usage:
+
+    Used Memory: ${TOTAL_MEM_USED}%
+    Free Memory: ${FREE_MEM}%
+_______________
+EOF
+
+
+
