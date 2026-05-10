f4bda602f111368168ef0f7b73949b4f2287462c
Sumit Nayak <sumit.debugged@gmail.com>
2026-05-10 22:37:09 +0530

    Add disk usage monitoring

--- server-stats.sh
+++ server-stats.sh
@@ -3,11 +3,15 @@
 TOTAL_CPU_USAGE=$(top -bn1 | awk '/Cpu\(s\)/ {print $2 + $4}')
 
 TOTAL_MEM_USED=$(free | awk '/Mem/ && !/Swap/ {printf "%.2f", ($3/$2)*100}')
+FREE_MEM=$(free | awk '/^Mem/ {printf "%.2f", ($4/$2)*100}')
 
-FREE_MEM=$(free | awk '/Mem/ && !/Swap/ {printf "%.2f", ($4/$2)*100}')
+TOTAL_SPACE_USAGE=$(df -h / | awk '/dev/ {print $5}')
+AVAILABLE_SPACE_PERCENTAGE=$(df -h / | awk '/dev/ {print 100 - $5}')
 
 cat << EOF
-______________
+
+                        SERVER STATS
+_______________________________________________________________
 
 Total CPU Usage: ${TOTAL_CPU_USAGE}%
 
@@ -15,7 +19,12 @@ Total Memory Usage:
 
     Used Memory: ${TOTAL_MEM_USED}%
     Free Memory: ${FREE_MEM}%
-_______________
+
+Total Disk Usage: 
+
+    Disk Usage: ${TOTAL_SPACE_USAGE}
+    Available Space: ${AVAILABLE_SPACE_PERCENTAGE}%
+_______________________________________________________________
 EOF
 
 
